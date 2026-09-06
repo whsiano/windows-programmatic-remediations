582821876545186aec49eb5d0a50f5c23a008615
Wilson <whsianoo@gmail.com>
2026-09-05 20:06:21 -0700

    Add PowerShell script to configure event log size
    This script sets the maximum size of the Windows Application event log to 32768 KB (32 MB) and ensures the registry key exists.

---
+++ STIGs/WN11-AU-000500.ps1
@@ -0,0 +1,43 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Josh Madakor
+    LinkedIn        : linkedin.com/in/joshmadakor/
+    GitHub          : github.com/joshmadakor1
+    Date Created    : 2024-09-09
+    Last Modified   : 2024-09-09
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-AU-000500
+    Documentation   : https://stigaview.com/products/win11/v2r7/WN11-AU-000500/
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#>
+
+# YOUR CODE GOES HERE
+
+#Requires -RunAsAdministrator
+
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Application"
+
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# MaxSize is in KB for this policy key. STIG requires 32768 KB (32 MB) or greater.
+New-ItemProperty -Path $regPath -Name "MaxSize" -Value 32768 -PropertyType DWord -Force | Out-Null
+
+$check = (Get-ItemProperty -Path $regPath -Name "MaxSize").MaxSize
+Write-Host "MaxSize is now $check KB" -ForegroundColor Green
