a08d22bdc95cb1407b6cee434464f2c0c427e44a
Wilson <whsianoo@gmail.com>
2026-09-05 20:08:24 -0700

    Modify author details and update documentation link
    Updated author information and documentation link.

--- STIGs/WN11-AU-000500.ps1
+++ STIGs/WN11-AU-000500.ps1
@@ -3,16 +3,16 @@
     This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
 
 .NOTES
-    Author          : Josh Madakor
-    LinkedIn        : linkedin.com/in/joshmadakor/
-    GitHub          : github.com/joshmadakor1
-    Date Created    : 2024-09-09
-    Last Modified   : 2024-09-09
+    Author          : Wilson Siano
+    LinkedIn        : linkedin.com/in/whsianoo/
+    GitHub          : github.com/whsiano
+    Date Created    : 2026-09-05
+    Last Modified   : 2026-09-05
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
     STIG-ID         : WN11-AU-000500
-    Documentation   : https://stigaview.com/products/win11/v2r7/WN11-AU-000500/
+    Documentation   : https://stigaview.com/products/win11/v2r3/WN11-AU-000500/
 
 .TESTED ON
     Date(s) Tested  : 
@@ -25,8 +25,6 @@
     Example syntax:
     PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
 #>
-
-# YOUR CODE GOES HERE
 
 #Requires -RunAsAdministrator
 
