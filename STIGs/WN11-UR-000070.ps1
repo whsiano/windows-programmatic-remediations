c7ff56dd3890507c66a4d0f017463cc5c925f9af
Wilson <whsianoo@gmail.com>
2026-09-06 14:22:19 -0700

    Update STIG-ID in WN11-UR-000070.ps1

--- STIGs/WN11-UR-000070.ps1
+++ STIGs/WN11-UR-000070.ps1
@@ -11,7 +11,7 @@
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
-    STIG-ID         : N/A
+    STIG-ID         : WN11-UR-000070
     Documentation   : https://stigaview.com/products/win11/v2r8/WN11-UR-000070/
 
 .TESTED ON
