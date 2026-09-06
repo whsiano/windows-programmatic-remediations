d78ff65d4522dfaf1f58ec54a58552a18e3166d7
Wilson <whsianoo@gmail.com>
2026-09-06 13:24:52 -0700

    Fix usage example in WN11-AU-000500.ps1
    Corrected example syntax in usage instructions.

--- STIGs/WN11-AU-000500.ps1
+++ STIGs/WN11-AU-000500.ps1
@@ -23,7 +23,7 @@
 .USAGE
     Put any usage instructions here.
     Example syntax:
-    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+    PS C:\> .\(STIG-ID-WN10-AU-000500).ps1 
 #>
 
 $regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Application"
